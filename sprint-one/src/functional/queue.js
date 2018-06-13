f50ef56f24b89a3d63fb988b5080b228608ad1af
frank <frankcorkery@gmail.com>
2018-06-13 19:04:15 -0400

    Working on debugging dequeue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[0] = value;
+    counter ++
   };
 
   someInstance.dequeue = function() {
+    if (counter > 0){
+      counter --;
+      console.log(storage[counter])
+      var dequeuer = storage[counter];
+      delete storage[counter];
+      return dequeuer;
+    }
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
