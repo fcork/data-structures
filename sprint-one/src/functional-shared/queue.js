67c64a5836faff44fd3410a7be9ac427ca3e69d0
frank <frankcorkery@gmail.com>
2018-06-14 07:44:13 -0400

    Updated both functional-shared and stack for prototypal

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  someInstance.first = 0;
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue: function(value){
+  		this.storage[this.counter++] = value;
+  	},
+  	dequeue: function(){
+  		if (this.first < this.counter){
+  		  var removed = this.storage[this.first];
+  		  delete this.storage[this.first];
+  		  this.first ++;
+  		  return removed;
+  	    }
+  	},
+  	size: function(){
+  		return this.counter - this.first;
+  	} 
+};
+
+
 
 
