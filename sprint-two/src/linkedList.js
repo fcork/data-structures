acb62da87486c481d6b31845fec2dc2c8614e6d1
frank <frankcorkery@gmail.com>
2018-06-14 16:10:28 -0400

    finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,39 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var counter = 0;
 
   list.addToTail = function(value) {
+    var tailNode = new Node(value);
+    this[counter++] = tailNode;
+    var oldTail = list.tail;
+    list.tail = tailNode;
+    if(oldTail !== null){
+      oldTail.next = tailNode;
+    } else {
+      list.head = tailNode;
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    list.head = list.head.next;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    if (currentNode.value === target){
+      return true;
+    } else {
+      while (currentNode.next){
+        currentNode = currentNode.next;
+         if(currentNode.value === target){
+           return true;
+         }
+      } 
+    }
+    return false;
   };
 
   return list;
