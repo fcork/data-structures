1b19893a6443ea8adcea3c0c4808d090e1fae90d
frank <frankcorkery@gmail.com>
2018-06-14 10:33:21 -0400

    Finished ES6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,28 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
   constructor() {
+    this.counter = 0;
+    this.first = 0;
   }
 
+  enqueue(value){
+    this[this.counter++] = value;
+  }
+
+  dequeue(){
+  	if (this.first < this.counter){
+      var removed = this[this.first];
+      
+      this.first++;
+      return removed;
+    }
+  }
+
+  size(){
+    return this.counter - this.first;
+  }
 }
+
+
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.counter = 0;
+  }
+
+  push(value){
+  	this[this.counter++] = value;
+  }
+
+  pop(){
+    if (this.counter > 0){
+  	  this.counter --;
+  	  var removed = this[this.counter];
+  	  delete this[this.counter];
+  	  return removed;
+    }
+  }
+  size(){
+  	return this.counter;
   }
 
 }
