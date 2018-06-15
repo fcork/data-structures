62388d751851931586c7f99ea39082d2f0566d1c
frank <frankcorkery@gmail.com>
2018-06-15 10:12:55 -0400

    Finsished tree using array to store children

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,31 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //this.children = {children: value};
+  var child = Tree(value);
+  //newTree.children[counter] = value
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target){
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++){
+      if (this.children[i].contains(target)){
+        return true;
+      };
+    }
+  }
+  return false;
 };
 
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
