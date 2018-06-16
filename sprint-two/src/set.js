180b045b824a974bda8d60eaf96854008810ab55
frank <frankcorkery@gmail.com>
2018-06-16 15:59:01 -0400

    Finished Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
+  var counter = {} // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage[item] === undefined) {
+    this._storage[item] = item
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this._storage){
+  	if (this._storage[key] === item){
+      return true;
+  	}
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (var key in this._storage){
+    if (this._storage[key] === item){
+      delete this._storage[key];
+    }
+  }
 };
 
 /*
