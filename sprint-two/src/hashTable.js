2ca6784c225170b7cc63340c73af3be08e3b809c
frank <frankcorkery@gmail.com>
2018-06-19 22:34:35 -0400

    Finished bare min requirements for hashTable and binarySearchTree

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,23 +2,30 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this.size = 0;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index)
-    if (!bucket){
-      this._storage.set(index,[k, v]);
-    } else {
-      _.each(bucket, function(hash, i){
-      	if (hash[i] === k){
-      		hash[i+1] === v;
-      	} else {
-      		bucket.push([k, v]);
-      	}
-      })
-  }
+  //var bucket = this._storage.get(index) || []
+  // this._storage.set(index, bucket)
+  var bucket = this._storage.get(index) || [];
+  
+    //for loop better because you can break out anytime you want
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === k){
+        var oldValue = tuple[1];
+        tuple[1] = v;
+        return oldValue;
+      }
+    } 
+        bucket.push([k, v]);
+        this._storage.set(index, bucket);
+
+
+    //this.storage.set()
   
 };
 
@@ -26,13 +33,13 @@ HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   if (!bucket){
-  	return undefined;
+    return undefined;
   }
   for (var i = 0; i < bucket.length; i++){
-  	var item = bucket[i];
-  	if (item[0] === k){
-  		return item[1];
-  	} 
+    var tuple = bucket[i];
+    if (tuple[0] === k){
+      return tuple[1];
+    } 
   }
 };
 
@@ -40,19 +47,37 @@ HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   if (!bucket) {
-  	return undefined;
+    return undefined;
   } else {
-  	if (bucket.length > 2){
       for (var i = 0; i < bucket.length; i++){
-      	var item = item[i];
-      	if(item === k){
-      		bucket.splice(i, 2);
-      	}
+        var tuple = bucket[i];
+        if(tuple[0] === k){
+          bucket.splice(i, 1);
+          this.size--
+          if(this.size < this.limit * 0.25) {
+            this.resize(0.5 * this.limit)
+          }
+          return tuple[1];
+        }
       }
-  	}
   }
 };
 
+
+// HashTable.prototype.resize = function(newLimit){
+//   var oldStorage = this.storage
+//   this._limit = newLimit;
+//   this.size = 0;
+//   this._storage = LimitedArray(newLimit);
+//   // var that = this;  Instead of binds, that.insert...
+//   oldStorage.each(function(bucket){
+//     if (bucket) {
+//       bucket.forEach(function(tuple){
+//         this.insert(tuple[0], tuple[1])
+//       }).bind(this)
+//     }
+//   }).bind(this)
+// }
 
 
 /*
